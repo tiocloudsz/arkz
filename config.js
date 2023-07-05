2ab5a93edbfc4aeb7dd65eb447d3a5ce89d13a9e
Tio <muhammadathiyatullah@gmail.com>
2023-07-06 01:23:22 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,52 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+global.apikey = 'SGWN' //https://api.lolhuman.xyz
+global.rosekey = '-' //https://api.itsrose.life
+//—————「 Set Nama Bot & Own 」—————//
+global.namabot = 'Arifzyn-MD V2.1'
+global.namaowner = 'ArifzynXD お-ぎ'
+
+//—————「 Setting Owner 」—————//
+global.owner = '62895347198105'
+global.ownernomer = ["62895347198105"]
+global.premium = ['62895347198105']
+
+//—————「 Set Wm 」—————//
+global.packname = ''
+global.author = 'Arifzxa お-ぎ'
+global.prefa = ['', '.']
+global.sp = '•'
+
+//—————「 Set Message 」—————//
+global.mess = {
+    done: 'Done ✅',
+    admin: 'Feature Only for _*Admin Group*_',
+    botAdmin: 'Perintah Ini Hanya Bisa Digunakan Ketika Bot Menjadi Admin Group !',
+    owner: 'Feature Only for _*owner*_',
+    group: 'Feature Only for _*Group Chat*_',
+    private: 'Feature Only for _*Admin Group*_',
+    wait: 'Wait a Moment, for Process',
+    endLimit: 'Your imit has run out, Wait at 12 at night',
+    error: '*!!!Feature Error!!!*',
+}
+
+//—————「 Set Limit 」—————//
+global.limitawal = {
+    premium: "Infinity",
+    free: 50,
+}
+
+//—————「 Set Image 」—————//
+global.imageurl = 'https://telegra.ph/file/54bdfdfaeadbda3984b82.jpg'
+global.isLink = `https://youtube.com/@arifzxa19`
+global.thumb = fs.readFileSync('./media/thumb.jpg')
+
+//—————「 Batas Akhir 」—————//
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+    fs.unwatchFile(file)
+    console.log(chalk.redBright(`Update'${__filename}'`))
+    delete require.cache[file]
+    require(file)
+})
